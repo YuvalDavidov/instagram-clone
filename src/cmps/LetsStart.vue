59230b10e582969ad4b978f90ac1a08319cfd4f8
Yuval <yuval838@gmail.com>
2023-03-12 13:25:33 +0200

    start

---
+++ src/cmps/LetsStart.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="header">
+    <h1>{{ txt }}</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      txt: "Starter",
+    };
+  },
+};
+</script>
